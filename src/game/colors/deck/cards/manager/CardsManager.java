39471cb87b72145c9f774bea79151faae7844f5e
Pradeep Rout <pradeeprout56@gmail.com>
2017-02-23 21:52:02 +0530

    Update CardsManager.java

--- src/game/colors/deck/cards/manager/CardsManager.java
+++ src/game/colors/deck/cards/manager/CardsManager.java
@@ -1,5 +1,7 @@
 package game.colors.deck.cards.manager;
 
+import java.util.List;
+
 import gae.colors.deck.cards.logger.CardLogger;
 import game.colors.deck.cards.player.CardPlayersVO;
 import game.colors.deck.cards.round.CardDeckAction;
@@ -10,8 +12,20 @@ public class CardsManager {
 	public static CardLogger cardLogger = new CardLogger();
 	public static void main(String[] args) {
 		cardPlayersVO = new CardPlayersVO();
-		CardDeckAction.dogetCardDeck(cardPlayersVO);
-		cardLogger.info(cardPlayersVO.getCard());
+		List<String> cardDeck = null;
+		try {
+			 cardDeck = CardDeckAction.dogetCardDeck(cardPlayersVO);
+		} catch (Exception e){
+			cardLogger.error(e.getMessage(), e);
+		}
+		String message = CardDeckAction.doGenerateMessage();
+		cardLogger.info(message + "" +cardPlayersVO.getCard());
+		List<String> shuffledCard = CardDeckAction.doGetShuffled(cardDeck, cardPlayersVO);
+		cardLogger.info(message + "" +shuffledCard);
+		String colorCard = CardDeckAction.doGetColorCard();
+		cardLogger.info(message + "" +colorCard);
+		cardLogger.info("color");
+		
 	}
 
 }
